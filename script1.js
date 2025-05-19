907dfd3dd248a28b287ef6d8b4a201a4ddf0363b
Pooja kc <poojakc994@gmail.com>
2025-05-19 20:47:34 +0545

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById("toggleTheme");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggleBtn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
